3581429586cab89a23a0fafb4712d153ee8ee3dc
Juan Brizuela <juanbrizuelago@gmail.com>
2022-05-14 11:21:23 -0500

    deleted json

--- js/firebase.js
+++ js/firebase.js
@@ -26,13 +26,18 @@ import {
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const data = fetch("../serviceAccountKey.json").then((response) => {
-  return response.json();
-});
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = await data;
+const firebaseConfig = {
+  apiKey: "AIzaSyBduob0U9SkmtNqjNcraGXLHEKvcUbPwI0",
+  authDomain: "mermelada-dashboard.firebaseapp.com",
+  databaseURL: "https://mermelada-dashboard-default-rtdb.firebaseio.com",
+  projectId: "mermelada-dashboard",
+  storageBucket: "mermelada-dashboard.appspot.com",
+  messagingSenderId: "116392436379",
+  appId: "1:116392436379:web:0783f6f9f27a9a63389c3e",
+  measurementId: "G-Y7SVC3XGCM"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
