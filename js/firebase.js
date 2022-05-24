f98bb8a0ef21e76416e656caf5de9051d25551db
Juan Brizuela <juanbrizuelago@gmail.com>
2022-05-24 13:25:16 -0500

    minor changes

--- js/firebase.js
+++ js/firebase.js
@@ -29,14 +29,14 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBduob0U9SkmtNqjNcraGXLHEKvcUbPwI0",
+  apiKey: process.env.API_KEY,
   authDomain: "mermelada-dashboard.firebaseapp.com",
   databaseURL: "https://mermelada-dashboard-default-rtdb.firebaseio.com",
   projectId: "mermelada-dashboard",
   storageBucket: "mermelada-dashboard.appspot.com",
   messagingSenderId: "116392436379",
   appId: "1:116392436379:web:0783f6f9f27a9a63389c3e",
-  measurementId: "G-Y7SVC3XGCM"
+  measurementId: "G-Y7SVC3XGCM",
 };
 
 // Initialize Firebase
